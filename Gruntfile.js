415cf675e3dbe15ad1fc7114f47b1b16e99497a0
Adi Hermawan <adidot78@gmail.com>
2019-12-04 12:37:34 +0700

    Gruntfile.js configuration

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    //  Task converting scss/sass to css (expanded)
+    sass: {
+      dist: {
+        options: { sourcemap: 'none' },
+        files: [{
+          expand: true,
+          cwd: './src/scss',
+          src: ['main-grid-layout.scss'],
+          dest: './dist/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    //  Minify css
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: './dist/css',
+          src: ['*.css', '!*.min.css'],
+          dest: './dist/css',
+          ext: '.min.css'
+        }]
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+
+  //  Register grunt task!
+  grunt.registerTask('default', ['sass', 'cssmin'])
+}
